649a77ba4ca52e3417d4281861733488b57ede32
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-24 18:50:08 -0200

    basic sample project

---
+++ tapioqueiro.py
@@ -0,0 +1,40 @@
+import tornado.ioloop
+from tornado.httpserver import HTTPServer
+
+from tapioca import TornadoRESTful, ResourceHandler
+
+
+class ProjectsHandler(ResourceHandler):
+
+    def get_collection(self, callback, *args, **kwargs):
+        callback([])
+
+    def get_model(self, key, *args, **kwargs):
+        """Gets an model instance"""
+        return {}
+
+    def update_model(self, key, *args, **kwargs):
+        self.application.db.users.update(key, self.values)
+
+
+class CommentsHandler(ResourceHandler):
+
+    def delete_model(self, key, *args, **kwargs):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    port = int(sys.argv[-1])
+    main_loop = tornado.ioloop.IOLoop.instance()
+
+    api = TornadoRESTful(
+            version='', base_url='http://127.0.0.1:{:d}'.format(port), discovery=True)
+    api.add_resource('projects', ProjectsHandler)
+    api.add_resource('comments', CommentsHandler)
+    application = tornado.web.Application(api.get_url_mapping())
+
+    server = HTTPServer(application)
+    server.bind(port, '127.0.0.1')
+    server.start(1)
+    main_loop.start()
